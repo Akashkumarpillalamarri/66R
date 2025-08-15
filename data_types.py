ee83be8d6e1d0bdb85faad073689cd6666903ba1
Akash Kumar <108814066+Akashkumarpillalamarri@users.noreply.github.com>
2025-08-15 18:55:18 +0530

    Update data_types.py

--- data_types.py
+++ data_types.py
@@ -11,99 +11,99 @@ print( "after swapping",a,b)'''
 
 #2 identify data types
 
-# c=input()
-# d=int(input()) 
-# e=float(input())
-# print("type is" ,type(c))
-# print("type is", type(d))
-# print("type is", type(e))
+c=input()
+d=int(input()) 
+e=float(input())
+print("type is" ,type(c))
+print("type is", type(d))
+print("type is", type(e))
 
 #3 simple calculator
-# f=23
-# g=7
-# print("addition",f+g)
-# print("subtraction" ,f-g)
-# print("multiplication",f*g)
-# print(  "division", f/g)
-# print("double division" ,f//g)
-# print("moduolus (remainder)", f%g)
+f=23
+g=7
+print("addition",f+g)
+print("subtraction" ,f-g)
+print("multiplication",f*g)
+print(  "division", f/g)
+print("double division" ,f//g)
+print("moduolus (remainder)", f%g)
 
 #4 salary hike
-# sal=15000
-# hike=15
-# new_sal=((hike/100)*sal)+sal
-# print("new salary is", new_sal)
+sal=15000
+hike=15
+new_sal=((hike/100)*sal)+sal
+print("new salary is", new_sal)
 
 
 #5 area and perimeter ofcircle
-# r=int(input())
-# area=(3.14159*(r**2))
-# print("area of circle is", area)
-# perimeter=2*3.14159*r
-# print("perimeter of circle is",perimeter)
+r=int(input())
+area=(3.14159*(r**2))
+print("area of circle is", area)
+perimeter=2*3.14159*r
+print("perimeter of circle is",perimeter)
 
 #6 ASCII
-# h=input()
-# print(ord(h))
+h=input()
+print(ord(h))
 
 #7
-# num=int(input())
-# if num%2==0:
-#     print("Even")
-# else:
-#     print("Odd")
+num=int(input())
+if num%2==0:
+    print("Even")
+else:
+    print("Odd")
 
 #8
-# i = 5
-# j = 8
+i = 5
+j = 8
 
-# i += 1
-# print("+= ", i)
+i += 1
+print("+= ", i)
 
-# j -= 1
-# print("-= ", j)
+j -= 1
+print("-= ", j)
 
-# i *= j
-# print("*= ", i)
+i *= j
+print("*= ", i)
 
-# i /= j
-# print("/= ", i)
+i /= j
+print("/= ", i)
 
-# # Integer division
-# i //= j
-# print("//= ", i)
+# Integer division
+i //= j
+print("//= ", i)
 
-# i %= j
-# print("%= ", i)
+i %= j
+print("%= ", i)
 
 
 
 #9 BOOLENA AND LOGICAL
-# a = 5
-# b = 7
-# print(a and b)
-# print(a or b)
-# print(not a)
+a = 5
+b = 7
+print(a and b)
+print(a or b)
+print(not a)
 
 
 # 10 bit wise shift pratice
-# a = 17
-# b = 5
-# print(a<<b)
-# print(a>>b)
+a = 17
+b = 5
+print(a<<b)
+print(a>>b)
 
 
 #11bitwise AND ,OR ,XOR, NOT
-# a=12
-# b=5
-# print(a&b)
-# print(a|b)
-# print(a^b)
-# print(~a) #NOT operator
+a=12
+b=5
+print(a&b)
+print(a|b)
+print(a^b)
+print(~a) #NOT operator
 
 
 
-#12 data type conversition
+##12 data type conversition
 # convert str to int
 a = "123"
 b=int(a)
@@ -194,4 +194,5 @@ print("n>>2",n>>2)
 #n<<1 means the bit wise operator move left 1 step 
 # 5 is 0101 after moving 1010 is 10
 #n>>2 means the bit wise operator move right 2 step
+
 # 5 is 0101 after moving 001 is 0
